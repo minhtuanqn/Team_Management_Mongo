6264ef857bda55ff49e41d262a35dcdc16a2b8ce
Tuan Minh Le <tuanmle@kms.com.vn>
2022-11-21 14:02:38 +0700

    update-test-office

--- src/test/java/com/nli/probation/unittest/service/OfficeServiceTest.java
+++ src/test/java/com/nli/probation/unittest/service/OfficeServiceTest.java
@@ -6,16 +6,20 @@ import static com.nli.probation.utils.TestUtils.createOfficeModel;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import com.nli.probation.constant.EntityStatusEnum.OfficeStatusEnum;
 import com.nli.probation.customexception.DuplicatedEntityException;
+import com.nli.probation.customexception.NoSuchEntityException;
 import com.nli.probation.entity.OfficeEntity;
 import com.nli.probation.model.office.CreateOfficeModel;
 import com.nli.probation.model.office.OfficeModel;
 import com.nli.probation.repository.OfficeRepository;
 import com.nli.probation.service.OfficeService;
 import com.nli.probation.service.SequenceGeneratorService;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
@@ -24,7 +28,8 @@ class OfficeServiceTest {
 
   private final OfficeRepository officeRepository = Mockito.mock(OfficeRepository.class);
   private final ModelMapper modelMapper = new ModelMapper();
-  private final SequenceGeneratorService sequenceGeneratorService = Mockito.mock(SequenceGeneratorService.class);
+  private final SequenceGeneratorService sequenceGeneratorService = Mockito.mock(
+      SequenceGeneratorService.class);
 
   /**
    * Create new office and save successfully
@@ -39,7 +44,8 @@ class OfficeServiceTest {
 
     OfficeModel expectedModel = createOfficeModel();
     CreateOfficeModel paramModel = createCreateOfficeModel();
-    OfficeModel actualModel = new OfficeService(officeRepository, modelMapper, sequenceGeneratorService).createOffice(paramModel);
+    OfficeModel actualModel = new OfficeService(officeRepository, modelMapper,
+        sequenceGeneratorService).createOffice(paramModel);
     assertTrue(compareTwoOffice(expectedModel, actualModel));
   }
 
@@ -51,9 +57,80 @@ class OfficeServiceTest {
     when(officeRepository.existsByName(anyString())).thenReturn(true);
 
     CreateOfficeModel paramModel = createCreateOfficeModel();
-    OfficeService officeService = new OfficeService(officeRepository, modelMapper, sequenceGeneratorService);
+    OfficeService officeService = new OfficeService(officeRepository, modelMapper,
+        sequenceGeneratorService);
     assertThrows(DuplicatedEntityException.class, () -> officeService.createOffice(paramModel));
   }
 
+  /**
+   * Delete a office and delete successfully
+   */
+  @Test
+  void when_deleteExistOffice_thenDeleteSuccessfully() {
+    OfficeModel officeModel = createOfficeModel();
+
+    OfficeEntity savedEntity = modelMapper.map(officeModel, OfficeEntity.class);
+    Optional<OfficeEntity> optional = Mockito.mock(Optional.class);
+    when(officeRepository.findById(anyInt())).thenReturn(optional);
+    when(optional.orElseThrow(any())).thenReturn(savedEntity);
+    when(officeRepository.save(any())).thenReturn(savedEntity);
+
+    OfficeModel expectedModel = createOfficeModel();
+    expectedModel.setStatus(OfficeStatusEnum.DISABLE.ordinal());
+
+    OfficeModel actualModel = new OfficeService(officeRepository, modelMapper,
+        sequenceGeneratorService).deleteOfficeById(savedEntity.getId());
+    assertTrue(compareTwoOffice(expectedModel, actualModel));
+  }
+
+  /**
+   * Delete office but can not find office by id
+   */
+  @Test
+  void when_DeleteNotExistOffice_thenThrowNoSuchEntityException() {
+    Optional<OfficeEntity> optional = Mockito.mock(Optional.class);
+    when(officeRepository.findById(any())).thenReturn(optional);
+    when(optional.orElseThrow(any())).thenThrow(NoSuchEntityException.class);
+
+    OfficeService officeService = new OfficeService(officeRepository, modelMapper,
+        sequenceGeneratorService);
+    assertThrows(NoSuchEntityException.class, () -> officeService.deleteOfficeById(1));
+  }
+
+  /**
+   * Find a office successfully
+   */
+  @Test
+  void when_findExistDepartment_thenReturnModelSuccessfully() {
+    OfficeModel officeModel = createOfficeModel();
+
+    OfficeEntity foundOffice = modelMapper.map(officeModel, OfficeEntity.class);
+    Optional<OfficeEntity> optional = Mockito.mock(Optional.class);
+    when(officeRepository.findById(anyInt())).thenReturn(optional);
+    when(optional.orElseThrow(any())).thenReturn(foundOffice);
+
+    OfficeModel expectedModel = createOfficeModel();
+
+    OfficeModel actualModel = new OfficeService(officeRepository, modelMapper,
+        sequenceGeneratorService).findOfficeById(
+        officeModel.getId());
+    assertTrue(compareTwoOffice(expectedModel, actualModel));
+  }
+
+  /**
+   * Find a office does not exist
+   */
+  @Test
+  void when_FindNotExistOffice_thenThrowNosuchEntity(){
+    OfficeModel officeModel = createOfficeModel();
+
+    Optional<OfficeEntity> optional = Mockito.mock(Optional.class);
+    when(officeRepository.findById(any())).thenReturn(optional);
+    when(optional.orElseThrow(any())).thenThrow(NoSuchEntityException.class);
+
+    OfficeService officeService = new OfficeService(officeRepository, modelMapper,
+        sequenceGeneratorService);
+    assertThrows(NoSuchEntityException.class, () -> officeService.findOfficeById(1));
+  }
 
 }
